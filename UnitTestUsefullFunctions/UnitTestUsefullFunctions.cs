86a0fd45b83f969706f6f59db7632ab905edf83d
Freddy Juhel <domaine75@gmail.com>
2015-02-27 09:48:52 +0100

    add unit test project and console application demo

---
+++ UnitTestUsefullFunctions/UnitTestUsefullFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestUsefullFunctions
+{
+  [TestClass]
+  public class UnitTestUsefullFunctions
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+
+    [TestMethod]
+    public void MyTestMethod()
+    {
+
+    }
+  }
+}
