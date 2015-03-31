bbfce90f480faa4d5d5b60924d867923a50321bf
Freddy Juhel <domaine75@gmail.com>
2015-03-31 15:02:32 +0200

    implementation of RandomString class

--- FonctionsUtiles.Fred.Csharp/RandomString.cs
+++ FonctionsUtiles.Fred.Csharp/RandomString.cs
@@ -7,13 +7,44 @@
 
     private char[] UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
     private char[] LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-    private char[] NumericCharacters = "0123456789".ToCharArray();
+    private char[] DigitCharacters = "0123456789".ToCharArray();
     private char[] SpecialCharacters = ",.;:?!/@#$%^&()=+*-_{}[]|~".ToCharArray();
     private char[] ForbiddenCharacters = new char[26 + 26 + 10 + 26]; // max size
+    private char[] SearchedCharacters = new char[26 + 26 + 10 + 26]; // max size
 
-    public RandomString(int numberOfCharacter)
+    public RandomString(char[] forbiddenCharacters, int numberOfCharacter = 1,
+      RandomCharacters rdnChar = RandomCharacters.LowerCase)
     {
       // TODO to be implemented
+      int numberOfCharactersToPickFrom = (int)rdnChar;
+      SearchedCharacters = new char[numberOfCharactersToPickFrom];
+      switch (rdnChar)
+      {
+        case RandomCharacters.LowerCase:
+          FillSearchedCharacters(LowerCaseCharacters);
+          break;
+        case RandomCharacters.UpperCase:
+          FillSearchedCharacters(UpperCaseCharacters);
+          break;
+        case RandomCharacters.Digit:
+          FillSearchedCharacters(DigitCharacters);
+          break;
+        case RandomCharacters.SpecialCharacter:
+          FillSearchedCharacters(SpecialCharacters);
+          break;
+        case RandomCharacters.UpperLower:
+          FillSearchedCharacters(UpperCaseCharacters);
+          FillSearchedCharacters(LowerCaseCharacters);
+          break;
+        
+        default:
+          break;
+      }
+    }
+
+    private void FillSearchedCharacters(char[] source)
+    {
+
     }
 
     public static string Generate()
@@ -22,5 +53,20 @@
 
       return result;
     }
+  }
+
+  public enum RandomCharacters
+  {
+    LowerCase = 2,
+    UpperCase = 4,
+    Digit = 6,
+    SpecialCharacter = 8,
+    UpperLower = LowerCase & UpperCase,
+    LowerDigit = LowerCase & Digit,
+    UpperDigit = UpperCase & Digit,
+    UpperLowerDigit = UpperLower & Digit,
+    LowerSpecialChar = LowerCase & SpecialCharacter,
+    UpperSpecialChar = UpperCase & SpecialCharacter,
+    DigitSpecialChar = Digit & SpecialCharacter
   }
 }
