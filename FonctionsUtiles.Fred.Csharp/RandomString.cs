b9dd75091532f9c0af815cf4cc15ebb113be3e7c
Freddy Juhel <domaine75@gmail.com>
2015-03-31 09:45:36 +0200

    add RandomString class

---
+++ FonctionsUtiles.Fred.Csharp/RandomString.cs
@@ -0,0 +1,26 @@
+﻿namespace FonctionsUtiles.Fred.Csharp
+{
+  class RandomString
+  {
+    public int NumberOfCharacter { get; set; }
+    public string RndString { get; set; }
+
+    private char[] UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    private char[] LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    private char[] NumericCharacters = "0123456789".ToCharArray();
+    private char[] SpecialCharacters = ",.;:?!/@#$%^&()=+*-_{}[]|~".ToCharArray();
+    private char[] ForbiddenCharacters = new char[26 + 26 + 10 + 26]; // max size
+
+    public RandomString(int numberOfCharacter)
+    {
+      // TODO to be implemented
+    }
+
+    public static string Generate()
+    {
+      string result = string.Empty;
+
+      return result;
+    }
+  }
+}
