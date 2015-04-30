5998c3535883d1addd4c9370c5c0d500e45313ad
Freddy Juhel <domaine75@gmail.com>
2015-04-30 13:17:30 +0200

    Remove unused using

--- FonctionsUtiles.Fred.Csharp/FunctionsMath.cs
+++ FonctionsUtiles.Fred.Csharp/FunctionsMath.cs
@@ -18,7 +18,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 using System.Security.Cryptography;
-using System.Numerics;
 
 namespace FonctionsUtiles.Fred.Csharp
 {
