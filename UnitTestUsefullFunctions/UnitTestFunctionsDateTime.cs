3205e37eeeea8942917acdf059fff365f995ce4c
Fred <domaine75@gmail.com>
2019-01-09 21:44:48 +0100

    Add leap years

--- UnitTestUsefullFunctions/UnitTestFunctionsDateTime.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsDateTime.cs
@@ -2319,6 +2319,7 @@ namespace UnitTestUsefullFunctions
     //Les années bissextiles du 21ème siècle
     //2004 - 2008 - 2012 - 2016 - 2020 - 2024 - 2028 - 2032 - 2036
     //2040 - 2044 - 2048 - 2052 - 2056 - 2060 - 2064 - 2068 - 2072
+    //2076 - 2080 - 2084 - 2088 - 2092 - 2096 - 2100
     
     [TestMethod]
     public void TestMethod_Bissextile_2004()
