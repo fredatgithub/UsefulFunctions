c953e4d6ecaa54860b0d1794a2b8ee8dc071ac47
fred <domaine75@gmail.com>
2019-01-26 23:05:01 +0100

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsDateTime.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsDateTime.cs
@@ -2439,6 +2439,15 @@ namespace UnitTestUsefullFunctions
         }
 
         [TestMethod]
+        public void TestMethod_Bissextile_2017()
+        {
+            DateTime source = new DateTime(2017, 1, 1);
+            const bool expected = false;
+            bool result = DateFunc.IsBissextile(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
         public void TestMethod_Bissextile_2019()
         {
             DateTime source = new DateTime(2019, 1, 1);
