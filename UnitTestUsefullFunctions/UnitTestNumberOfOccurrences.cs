b622a35c9927f17bcc5610c85e335a8f633db683
Fred <domaine75@gmail.com>
2019-02-16 10:55:23 +0100

    Add unit test

--- UnitTestUsefullFunctions/UnitTestNumberOfOccurrences.cs
+++ UnitTestUsefullFunctions/UnitTestNumberOfOccurrences.cs
@@ -195,5 +195,15 @@ namespace UnitTestUsefullFunctions
       int result = StringFunc.NumberOfOccurrences(source1, source2);
       Assert.AreEqual(result, expected);
     }
+
+    [TestMethod]
+    public void TestMethod_NumberOfOccurrences_brown_fox_f_1()
+    {
+      const string source1 = "f";
+      const string source2 = "The quick brown fox jumps over the lazy dog";
+      const int expected = 1;
+      int result = StringFunc.NumberOfOccurrences(source1, source2);
+      Assert.AreEqual(result, expected);
+    }
   }
 }
