161baa9aefdc7a5ddb94b93467f6ba0b2f514e74
fred <domaine75@gmail.com>
2019-04-17 21:44:15 +0200

    Update AssemblyInfo.cs

--- CodeGeneration/Properties/AssemblyInfo.cs
+++ CodeGeneration/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CodeGeneration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("my Company")]
 [assembly: AssemblyProduct("CodeGeneration")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyCopyright("Copyright © my Company 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
