4d93f7f08c66259d0e2e37b37417bb9e03476be2
fred <domaine75@gmail.com>
2019-07-09 11:23:09 +0200

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
@@ -1080,6 +1080,15 @@ namespace UnitTestUsefullFunctions
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_IsPrime_523()
+    {
+      const int source = 523;
+      const bool expected = true;
+      bool result = FunctionsMath.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
     /*  Add unit test
     De 501 à 600:
     503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599
