1d190276db1af368241f26baa8587e0616e51a3d
fred <domaine75@gmail.com>
2019-09-27 10:48:17 +0200

    Add unit tetst

--- UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
@@ -1774,6 +1774,15 @@ namespace UnitTestUsefullFunctions
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_IsPrime_1049()
+    {
+      const int source = 1049;
+      const bool expected = true;
+      bool result = FunctionsMath.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
 
     /*  Add unit test
      TOUS LES NOMBRES PREMIERS DE 1001 À 2000
