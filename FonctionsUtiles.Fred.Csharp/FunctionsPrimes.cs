f8c7e5ee24e5364ade6610cac1890b8077f503a0
fred <domaine75@gmail.com>
2019-10-03 19:12:10 +0200

    Add prime methods

--- FonctionsUtiles.Fred.Csharp/FunctionsPrimes.cs
+++ FonctionsUtiles.Fred.Csharp/FunctionsPrimes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace FonctionsUtiles.Fred.Csharp
 {
@@ -133,6 +134,73 @@ namespace FonctionsUtiles.Fred.Csharp
       }
 
       return result;
+    }
+
+    public static string Negate(string verb, bool positive)
+    {
+      return positive ? verb : $"n'{verb} pas";
+    }
+
+    public static bool IsPrime(ulong number)
+    {
+      if (number <= 1)
+      {
+        return false;
+      }
+
+      if (number == 2 || number == 3 || number == 5)
+      {
+        return true;
+      }
+
+      if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+      {
+        return false;
+      }
+
+      for (ulong divisor = 7; divisor < Math.Sqrt(number); divisor += 2)
+      {
+        if (number % divisor == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    public static bool IsPrime(BigInteger number)
+    {
+      if (number.IsEven)
+      {
+        return false;
+      }
+
+      if (number.Sign == 0 || number.Sign == -1)
+      {
+        return false;
+      }
+
+      if (number == 2 || number == 3 || number == 5)
+      {
+        return true;
+      }
+
+      if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+      {
+        return false;
+      }
+
+      var squareRoot = Math.Exp(BigInteger.Log(number) / 2);
+      for (ulong divisor = 7; divisor < squareRoot; divisor += 2)
+      {
+        if (number % divisor == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
     }
   }
 }
