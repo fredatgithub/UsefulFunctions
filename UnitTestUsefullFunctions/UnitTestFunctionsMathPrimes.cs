dc750731d77a28e95c3969623d0eb2b7d0b7a6f2
fred <domaine75@gmail.com>
2019-10-15 09:26:55 +0200

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
@@ -1936,6 +1936,15 @@ namespace UnitTestUsefullFunctions
             Assert.AreEqual(result, expected);
         }
 
+        [TestMethod]
+        public void TestMethod_IsPrime_1187()
+        {
+            const int source = 1187;
+            const bool expected = true;
+            bool result = FunctionsMath.IsPrime(source);
+            Assert.AreEqual(result, expected);
+        }
+
 
         /*  Add unit test
          *  
