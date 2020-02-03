c782700ca5ac5e1728974f7e643792c20790d69d
Fred <domaine75@gmail.com>
2020-02-04 00:21:43 +0100

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsMathPrimes.cs
@@ -2881,10 +2881,19 @@ namespace UnitTestUsefullFunctions
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_IsPrime_1987()
+    {
+      const int source = 1987;
+      const bool expected = true;
+      bool result = FunctionsMath.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
     /*  Add unit test
      *  
      *  TOUS LES NOMBRES PREMIERS DE 1001 À 2000
-     1 979 1 987 1 993 1 997 1 999
+     1 987 1 993 1 997 1 999
 
 
 TOUS LES NOMBRES PREMIERS DE 2001 À 3000
