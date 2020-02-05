cb8659f3a392f0e7b1aeb848e37d546704c7f383
Fred <domaine75@gmail.com>
2020-02-05 23:21:39 +0100

    Refactor code

--- ConsoleAppCalendar/Program.cs
+++ ConsoleAppCalendar/Program.cs
@@ -23,11 +23,11 @@ namespace ConsoleAppCalendar
       Console.ReadKey();
     }
 
-    private static string GetCalendar(string order)
+    public static string GetCalendar(string order)
     {
       string result = string.Empty;
-      result = " Sun   Mon   Tue   Wed   Thu   Fri   Sat" + Environment.NewLine;
-      result += "---   ---   ---   ---   ---   ---   ---" + Environment.NewLine;
+      result = GetHeader();
+      result += UnderlineHeader();
       result += "  26   27    28    29    30    31     1" + Environment.NewLine;
       result += "   2    3     4     5     6     7     8" + Environment.NewLine;
       result += "   9   10    11    12    13    14    15" + Environment.NewLine;
@@ -35,6 +35,38 @@ namespace ConsoleAppCalendar
       result += "  23   24    25    26    27    28    29" + Environment.NewLine;
 
       return result;
+    }
+
+    public static string GetHeader(string language = "en-us", string startWeek = "sun")
+    {
+      string result = string.Empty;
+      switch (language)
+      {
+        case "en-us":
+          result = result = " Sun   Mon   Tue   Wed   Thu   Fri   Sat" + Environment.NewLine;
+          break;
+        case "en-uk":
+          result = result = " Sun   Mon   Tue   Wed   Thu   Fri   Sat" + Environment.NewLine;
+          break;
+        case "fr-fr":
+          result = result = " Dim   Lun   Mar   Mer   Jeu   Ven   Sam" + Environment.NewLine;
+          break;
+        default:
+          result = result = " Sun   Mon   Tue   Wed   Thu   Fri   Sat" + Environment.NewLine;
+          break;
+      }
+
+      return result;
+    }
+
+    public static string UnderlineHeader()
+    {
+      return " ---   ---   ---   ---   ---   ---   ---" + Environment.NewLine;
+    }
+
+    public static string NewLine()
+    {
+      return Environment.NewLine;
     }
   }
 }
