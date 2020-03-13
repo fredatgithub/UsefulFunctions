31e4f80227b07f74b266b5fdb80d798929fd5ef3
Fred <domaine75@gmail.com>
2020-03-13 17:54:40 +0100

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsPrimes.cs
@@ -598,6 +598,15 @@ namespace UnitTestUsefullFunctions
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_Pi_167()
+    {
+      const int source = 167;
+      const int expected = 39;
+      int result = FunctionsPrimes.Pi(source);
+      Assert.AreEqual(result, expected);
+    }
+
 
     /* Primes between 2 and 4000
       2      3      5      7     11     13     17     19     23     29 
