cb64a13c03b399280a8e3146656c46a95d12147b
Fred <domaine75@gmail.com>
2020-03-21 15:16:51 +0100

    Uncomment searching for triplet prime with Big integer

--- ConsoleAppPrimesByHundred/Program.cs
+++ ConsoleAppPrimesByHundred/Program.cs
@@ -1,84 +1,80 @@
 ﻿using FonctionsUtiles.Fred.Csharp;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.InteropServices;
 
 
 namespace ConsoleAppPrimesByHundred
 {
-    internal class Program
+  internal class Program
+  {
+    private static void Main()
     {
-        private static void Main()
+      Action<string> display = Console.WriteLine;
+      display("Prime numbers by hundred:");
+      foreach (var kvp in FunctionsPrimes.NumberOfPrimesByHundred(1000))
+      {
+        display($"{kvp.Key} - {kvp.Value}");
+      }
+
+      display(string.Empty);
+      display("Prime numbers by thousand:");
+      display(string.Empty);
+      foreach (var kvp in FunctionsPrimes.NumberOfPrimesByNthHundred(9000, 1000))
+      {
+        display($"{kvp.Key} - {kvp.Value}");
+      }
+
+      //int count = 0;
+      //for (int i = 3; i <= int.MaxValue - 4; i += 2)
+      //{
+      //  if (FunctionsPrimes.IsPrimeTriplet(i))
+      //  {
+      //    count++;
+      //  }
+
+      //  display($"Number of prime found: {count} and i: {i}");
+      //}
+
+      display("no triplet prime from 3 to 2147483643");
+      int count = 0;
+      List<string> result = new List<string>();
+      for (BigInteger i = BigInteger.Parse("2147483643"); i <= BigInteger.Parse("4147483643"); i += 2)
+      {
+        if (FunctionsPrimes.IsPrimeTriplet(i))
         {
-            Action<string> display = Console.WriteLine;
-            display("Prime numbers by hundred:");
-            foreach (var kvp in FunctionsPrimes.NumberOfPrimesByHundred(1000))
-            {
-                display($"{kvp.Key} - {kvp.Value}");
-            }
-
-            display(string.Empty);
-            display("Prime numbers by thousand:");
-            display(string.Empty);
-            foreach (var kvp in FunctionsPrimes.NumberOfPrimesByNthHundred(9000, 1000))
-            {
-                display($"{kvp.Key} - {kvp.Value}");
-            }
-
-            //int count = 0;
-            //for (int i = 3; i <= int.MaxValue - 4; i += 2)
-            //{
-            //  if (FunctionsPrimes.IsPrimeTriplet(i))
-            //  {
-            //    count++;
-            //  }
-
-            //  display($"Number of prime found: {count} and i: {i}");
-            //}
-
-            display("no triplet prime from 3 to 2147483643");
-            //int count = 0;
-            //List<string> result = new List<string>();
-            //for (BigInteger i = BigInteger.Parse("2147483643"); i <= BigInteger.Parse("9147483643"); i += 2)
-            //{
-            //  if (FunctionsPrimes.IsPrimeTriplet(i))
-            //  {
-            //    count++;
-            //    result.Add(i.ToString());
-            //  }
-
-            //  display($"Number of prime found: {count} and i: {i}");
-            //}
-
-            //if (result.Count > 0)
-            //{
-            //  foreach (string number in result)
-            //  {
-            //    display($"Prime triplet found : {number}");
-            //  }
-            //}
-
-
-
-            var timestamp = DateTime.Now.ToFileTime();
-            display($"time stamp using ToFileTime : {timestamp.ToString()}");
-
-            string timeStamp = GetTimestamp(DateTime.Now);
-            display($"time stamp yyyymmddhh : {timestamp.ToString()}");
-
-            display("Press any key to exit");
-            Console.ReadKey();
+          count++;
+          result.Add(i.ToString());
         }
 
-        public static String GetTimestamp(DateTime value)
+        display($"Number of prime found: {count} and i = {i}");
+      }
+
+      if (result.Count > 0)
+      {
+        foreach (string number in result)
         {
-            return value.ToString("yyyyMMddHHmmssffff");
+          display($"Prime triplet found : {number}");
         }
+      }
 
 
+
+      var timestamp = DateTime.Now.ToFileTime();
+      display($"time stamp using ToFileTime : {timestamp.ToString()}");
+
+      string timeStamp = GetTimestamp(DateTime.Now);
+      display($"time stamp yyyymmddhh : {timestamp.ToString()}");
+
+      display("Press any key to exit");
+      Console.ReadKey();
     }
+
+    public static String GetTimestamp(DateTime value)
+    {
+      return value.ToString("yyyyMMddHHmmssffff");
+    }
+
+
+  }
 }
