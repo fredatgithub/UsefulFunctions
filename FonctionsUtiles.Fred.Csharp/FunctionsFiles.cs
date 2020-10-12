3768704c9a7eff2c68659d372c3aca1b7a7ae58a
Fred <domaine75@gmail.com>
2020-10-13 00:08:11 +0200

    Add Is-file-Locked method

--- FonctionsUtiles.Fred.Csharp/FunctionsFiles.cs
+++ FonctionsUtiles.Fred.Csharp/FunctionsFiles.cs
@@ -850,5 +850,31 @@ namespace FonctionsUtiles.Fred.Csharp
 
       throw new NotImplementedException();
     }
+
+    public static bool IsFileLocked(FileInfo file)
+    {
+      try
+      {
+        using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+        {
+          stream.Close();
+        }
+      }
+      catch (IOException)
+      {
+        //the file is unavailable because it is:
+        //still being written to
+        //or being processed by another thread
+        //or does not exist (has already been processed)
+        return true;
+      }
+      catch (Exception)
+      {
+        return true;
+      }
+
+      //file is not locked
+      return false;
+    }
   }
 }
