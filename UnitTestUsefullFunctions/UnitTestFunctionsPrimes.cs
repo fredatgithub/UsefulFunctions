784ec7d9cb95878923ed3e9a7ae07756ddb87eaa
fred <domaine75@gmail.com>
2021-06-05 18:36:27 +0200

    Update UnitTestFunctionsPrimes.cs

--- UnitTestUsefullFunctions/UnitTestFunctionsPrimes.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsPrimes.cs
@@ -982,9 +982,19 @@ namespace UnitTestUsefullFunctions
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_Pi_389()
+    {
+      const int source = 389;
+      const int expected = 77;
+      int result = FunctionsPrimes.Pi(source);
+      Assert.AreEqual(result, expected);
+    }
+    
+    
     /* Primes between 2 and 4000
       
-    383    389    397    401    409 
+    389    397    401    409 
     419    421    431    433    439    443    449    457    461    463 
     467    479    487    491    499    503    509    521    523    541 
     547    557    563    569    571    577    587    593    599    601 
