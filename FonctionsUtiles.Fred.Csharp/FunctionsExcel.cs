3ed9ada4a26a58a35d59b935f1b0d9b9a947e9e5
Fred <domaine75@gmail.com>
2021-07-21 09:18:47 +0200

    Add Excel methods and its unit tests

---
+++ FonctionsUtiles.Fred.Csharp/FunctionsExcel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace FonctionsUtiles.Fred.Csharp
+{
+  public static class FunctionsExcel
+  {
+    public static List<string> AllColumnsNameUntil(int lastColumnUsed)
+    {
+      List<string> result = new List<string>();
+      if (lastColumnUsed < 1)
+      {
+        return result;
+      }
+
+      if (lastColumnUsed <= 26)
+      {
+        for (int i = 65; i <= 65 + 26; i++)
+        {
+          result.Add(((char)i).ToString());
+          if (i - 64 == lastColumnUsed)
+          {
+            break;
+          }
+        }
+
+        return result;
+      }
+
+      if (lastColumnUsed >= 27 && lastColumnUsed <= 52)
+      {
+        result = new List<string>(AllColumnsNameUntil(26));
+        for (char letter = 'A'; letter <= lastColumnUsed - 26; letter++)
+        {
+          result.Add($"A{letter}");
+        }
+
+        return result;
+      }
+
+      if (lastColumnUsed >= 53 && lastColumnUsed <= 79)
+      {
+        result = new List<string>(AllColumnsNameUntil(52));
+        for (char letter = 'A'; letter < lastColumnUsed - 52; letter++)
+        {
+          result.Add($"B{letter}");
+        }
+
+        return result;
+      }
+
+      return result;
+    }
+
+    public static List<string> AlphabetLetters()
+    {
+      List<string> result = new List<string>();
+      for (char letter = 'A'; letter <= 'Z'; letter++)
+      {
+        result.Add(letter.ToString());
+      }
+
+      return result;
+    }
+  }
+}
---
+++ UnitTestUsefullFunctions/UnitTestFunctionsExcel.cs
@@ -0,0 +1,79 @@
+﻿using FonctionsUtiles.Fred.Csharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestUsefullFunctions
+{
+  /// <summary>
+  /// Description résumée pour UnitTestFunctionsExcel
+  /// </summary>
+  [TestClass]
+  public class UnitTestFunctionsExcel
+  {
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_0()
+    {
+      const int source = 0;
+      List<string> expected = new List<string>();
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_1()
+    {
+      const int source = 1;
+      List<string> expected = new List<string>
+      {
+        "A"
+      };
+
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
+
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_2()
+    {
+      const int source = 2;
+      List<string> expected = new List<string>
+      {
+        "A", "B"
+      };
+
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
+
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_26()
+    {
+      const int source = 26;
+      List<string> expected = new List<string>
+      {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+      };
+
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
+
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_27()
+    {
+      const int source = 27;
+      List<string> expected = new List<string>
+      {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA"
+      };
+
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
+  }
+}
