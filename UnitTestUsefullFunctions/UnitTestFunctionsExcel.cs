aac8829bd0dda655ce2e7b6c4fb040b2ece19776
Fred <domaine75@gmail.com>
2021-07-24 11:08:54 +0200

    Add unit test

--- UnitTestUsefullFunctions/UnitTestFunctionsExcel.cs
+++ UnitTestUsefullFunctions/UnitTestFunctionsExcel.cs
@@ -89,5 +89,19 @@ namespace UnitTestUsefullFunctions
       List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
       CollectionAssert.AreEquivalent(result, expected);
     }
+
+    [TestMethod]
+    [TestCategory("Excel")]
+    public void TestMethod_AllColumnsUntil_29()
+    {
+      const int source = 29;
+      List<string> expected = new List<string>
+      {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC"
+      };
+
+      List<string> result = FunctionsExcel.AllColumnsNameUntil(source);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
   }
 }
