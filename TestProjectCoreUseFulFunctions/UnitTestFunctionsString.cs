932cc120cca843f5a62e9d5518afaf9b231fab36
Fred <domaine75@gmail.com>
2021-10-28 15:59:25 +0200

    Add one unit test to a new method

---
+++ TestProjectCoreUseFulFunctions/UnitTestFunctionsString.cs
@@ -0,0 +1,22 @@
+﻿using FonctionsUtiles.Fred.Csharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestProjectCoreUseFulFunctions
+{
+  [TestClass]
+  public class UnitTestFunctionsString
+  {
+    [DataTestMethod]
+    [TestCategory("String")]
+    
+    public void TestMethod_SplitString_One_Word()
+    {
+      string source = "azerty";
+      const int source2 = 3;
+      List<string> expected = new List<string>() { "az", "rt", "" };
+      List<string> result = (List<string>)FunctionsString.SplitString(source, source2);
+      CollectionAssert.AreEquivalent(result, expected);
+    }
+  }
+}
