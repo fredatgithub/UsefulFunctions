27899e286a67210188c91eb8f84cbd33d0e8c474
Fred <domaine75@gmail.com>
2022-11-02 02:05:33 +0100

    Add unit test

--- TestProjectCoreUseFulFunctions/UnitTestFunctionsMathPrime.cs
+++ TestProjectCoreUseFulFunctions/UnitTestFunctionsMathPrime.cs
@@ -8,7 +8,7 @@ namespace TestProjectCoreUseFulFunctions
   {
     /*
   Liste des nombres premiers allant de 1 à 10000
-    13, 
+
     17, 
     19, 
     23, 
@@ -23,7 +23,7 @@ namespace TestProjectCoreUseFulFunctions
     [DataRow(5)]
     [DataRow(7)]
     [DataRow(11)]
-    [DataRow(11)]
+    [DataRow(13)]
     [DataRow(2819)]
     [DataRow(2833)]
     [DataRow(2837)]
