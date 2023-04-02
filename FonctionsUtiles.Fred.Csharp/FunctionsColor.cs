3075944e979bd966f9652f11cf07373b7df5a65b
Fred <domaine75@gmail.com>
2023-04-02 18:20:37 +0200

    Add color methods

---
+++ FonctionsUtiles.Fred.Csharp/FunctionsColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace FonctionsUtiles.Fred.Csharp
+{
+  public static class FunctionsColor
+  {
+    public static Color RandomColor()
+    {
+      var _random = new Random(DateTime.Now.Millisecond);
+      return Color.FromArgb(_random.Next(255), _random.Next(255), _random.Next(255));
+    }
+  }
+}
