683921d98297e911b87aeeaea64d92fcdb5988ff
Fred <domaine75@gmail.com>
2023-05-30 00:30:54 +0200

    Add enum

---
+++ FonctionsUtiles.Fred.Csharp/Enums/DateFormat.cs
@@ -0,0 +1,15 @@
+﻿namespace FonctionsUtiles.Fred.Csharp.Enums
+{
+  public enum DateFormat
+  {
+    /// <summary>
+    /// Dates are written in the ISO 8601 format, e.g. <c>"2012-03-21T05:40Z"</c>.
+    /// </summary>
+    IsoDateFormat,
+
+    /// <summary>
+    /// Dates are written in the Microsoft JSON format, e.g. <c>"\/Date(1198908717056)\/"</c>.
+    /// </summary>
+    MicrosoftDateFormat
+  }
+}
