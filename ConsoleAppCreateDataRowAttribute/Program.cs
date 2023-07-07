4d45a73c01a206613da75cc07c3c0588a449e265
Fred <domaine75@gmail.com>
2023-07-08 00:00:05 +0200

    add console app for listing datarow attribute

---
+++ ConsoleAppCreateDataRowAttribute/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCreateDataRowAttribute
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      var start = 4_177;
+      var end = 4_999;
+      var result = new list<int>();
+      for (int i = start; i <= end; i++)
+      {
+        if (true)
+        {
+
+        }
+      }
+    }
+  }
+}
