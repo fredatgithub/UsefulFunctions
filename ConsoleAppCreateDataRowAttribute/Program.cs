a0f629bd4fa42d57a05e3d6f27c584059c5664cb
Fred <domaine75@gmail.com>
2023-07-08 00:09:49 +0200

    add unit tests for prime numbers from 4000 to 5k

--- ConsoleAppCreateDataRowAttribute/Program.cs
+++ ConsoleAppCreateDataRowAttribute/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using FonctionsUtiles.Fred.Csharp;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppCreateDataRowAttribute
 {
@@ -12,14 +10,24 @@ namespace ConsoleAppCreateDataRowAttribute
     {
       var start = 4_177;
       var end = 4_999;
-      var result = new list<int>();
+      var result = new List<string>();
       for (int i = start; i <= end; i++)
       {
-        if (true)
+        if (FunctionsPrimes.IsPrime(i))
         {
-
+          // [DataRow(4177)]
+          result.Add($"[DataRow({i})]");
         }
       }
+
+      foreach (var i in result)
+      {
+        Console.WriteLine(i);
+      }
+
+      Console.WriteLine();
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadLine();
     }
   }
 }
