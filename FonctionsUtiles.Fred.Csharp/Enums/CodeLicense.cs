7fa71fdfa83b6dde18bdba66193e268bf0abd904
Fred <domaine75@gmail.com>
2023-09-23 00:49:54 +0200

    Add enumerations

---
+++ FonctionsUtiles.Fred.Csharp/Enums/CodeLicense.cs
@@ -0,0 +1,94 @@
+﻿namespace FonctionsUtiles.Fred.Csharp.Enums
+{
+  /// <summary>
+  /// Licenses than can be searched on GitHub
+  /// https://help.github.com/articles/searching-repositories#search-by-license
+  /// </summary>
+  public enum License
+  {
+    [Parameter(Value = "afl-3.0")]
+    AcademicFree_3_0,
+    [Parameter(Value = "apache-2.0")]
+    Apache_2_0,
+    [Parameter(Value = "artistic-2.0")]
+    Artistic_2_0,
+    [Parameter(Value = "bsl-1.0")]
+    BoostSoftware_1_0,
+    [Parameter(Value = "0bsd")]
+    BSD0Clause,
+    [Parameter(Value = "bsd-2-clause")]
+    BSD2Clause,
+    [Parameter(Value = "bsd-3-clause")]
+    BSD3Clause,
+    [Parameter(Value = "bsd-3-clause-clear")]
+    BSD3ClauseClear,
+    [Parameter(Value = "bsd-4-clause")]
+    BSD4Clause,
+    [Parameter(Value = "cc")]
+    CreativeCommons,
+    [Parameter(Value = "cc0-1.0")]
+    CreativeCommonsZero_1_0,
+    [Parameter(Value = "cc-by-4.0")]
+    CreativeCommonsAtrribution_4_0,
+    [Parameter(Value = "cc-by-sa-4.0")]
+    CreativeCommonsAttributionShareAlike_4_0,
+    [Parameter(Value = "cecill-2.1")]
+    CeCILL_2_1,
+    [Parameter(Value = "wtfpl")]
+    DoWhatTheFYouWant,
+    [Parameter(Value = "ecl-2.0")]
+    EducationalCommunity_2_0,
+    [Parameter(Value = "epl-1.0")]
+    EclipsePublic_1_0,
+    [Parameter(Value = "epl-2.0")]
+    EclipsePublic_2_0,
+    [Parameter(Value = "eupl-1.1")]
+    EuropeanUnionPublic_1_1,
+    [Parameter(Value = "eupl-1.2")]
+    EuropeanUnionPublic_1_2,
+    [Parameter(Value = "agpl-3.0")]
+    GNUAfferoGeneralPublic_3_0,
+    [Parameter(Value = "gpl")]
+    GNUGeneralPublic,
+    [Parameter(Value = "gpl-2.0")]
+    GNUGeneralPublic_2_0,
+    [Parameter(Value = "gpl-3.0")]
+    GNUGeneralPublic_3_0,
+    [Parameter(Value = "lgpl")]
+    GNULesserGeneralPublic,
+    [Parameter(Value = "lgpl-2.1")]
+    GNULesserGeneralPublic_2_1,
+    [Parameter(Value = "lgpl-3.0")]
+    GNULesserGeneralPublic_3_0,
+    [Parameter(Value = "isc")]
+    ISC,
+    [Parameter(Value = "lppl-1.3c")]
+    LatexProjectPublic,
+    [Parameter(Value = "ms-pl")]
+    MicrosoftPublic,
+    [Parameter(Value = "ms-rl")]
+    MicrosoftReciprocal,
+    [Parameter(Value = "mit")]
+    MIT,
+    [Parameter(Value = "mpl-2.0")]
+    MozillaPublic_2_0,
+    [Parameter(Value = "odbl-1.0")]
+    ODCOpenDatabase,
+    [Parameter(Value = "osl-3.0")]
+    OpenSoftware_3_0,
+    [Parameter(Value = "postgresql")]
+    PostgreseSQL,
+    [Parameter(Value = "ofl-1.1")]
+    SILOpenFont,
+    [Parameter(Value = "upl-1.0")]
+    UniversalPermissive,
+    [Parameter(Value = "ncsa")]
+    NSCAOpenSource,
+    [Parameter(Value = "unlicense")]
+    TheUnlicense,
+    [Parameter(Value = "vim")]
+    Vim,
+    [Parameter(Value = "zlib")]
+    ZLib
+  }
+}
---
+++ FonctionsUtiles.Fred.Csharp/Enums/Language.cs
@@ -0,0 +1,258 @@
+﻿namespace FonctionsUtiles.Fred.Csharp.Enums
+{
+  /// <summary>
+  /// Languages that can be searched on in GitHub
+  /// https://help.github.com/articles/searching-repositories#languages
+  /// </summary>
+  public enum Language
+  {
+    Abap,
+    [Parameter(Value = "ActionScript")]
+    ActionScript,
+    Ada,
+    Apex,
+    [Parameter(Value = "AppleScript")]
+    AppleScript,
+    Arc,
+    Arduino,
+    [Parameter(Value = "ApacheConf")]
+    ApacheConf,
+    Asp,
+    Assembly,
+    Augeas,
+    [Parameter(Value = "AutoHotkey")]
+    AutoHotKey,
+    Awk,
+    Batchfile,
+    Befunge,
+    [Parameter(Value = "BlitzMax")]
+    BlitzMax,
+    Boo,
+    Bro,
+    C,
+    [Parameter(Value = "C2HS Haskell")]
+    C2hsHaskell,
+    Ceylon,
+    Chuck,
+    Clips,
+    Clojure,
+    Cobol,
+    Cmake,
+    [Parameter(Value = "C-ObjDump")]
+    CObjDump,
+    [Parameter(Value = "CoffeeScript")]
+    CoffeeScript,
+    [Parameter(Value = "ColdFusion")]
+    ColdFusion,
+    CommonLisp,
+    Coq,
+    [Parameter(Value = "cpp")]
+    CPlusPlus,
+    [Parameter(Value = "CSharp")]
+    CSharp,
+    Css,
+    [Parameter(Value = "Cpp-ObjDump")]
+    CppObjDump,
+    Cucumber,
+    Cython,
+    D,
+    [Parameter(Value = "D-ObjDump")]
+    DObjDump,
+    [Parameter(Value = "DarcsPatch")]
+    DarcsPatch,
+    Dart,
+    [Parameter(Value = "DCPU-16 ASM")]
+    Dcpu16Asm,
+    Dot,
+    Dylan,
+    Ec,
+    [Parameter(Value = "Ecere Projects")]
+    EcereProjects,
+    Ecl,
+    Edn,
+    Eiffel,
+    Elixir,
+    Elm,
+    EmacsLisp,
+    Erlang,
+    [Parameter(Value = "F#")]
+    FSharp,
+    Factor,
+    Fancy,
+    Fantom,
+    Fish,
+    Forth,
+    Fortran,
+    Gas,
+    Genshi,
+    [Parameter(Value = "Gentoo Build")]
+    GentooBuild,
+    [Parameter(Value = "Gentoo Eclass")]
+    GentooEclass,
+    [Parameter(Value = "Gettext Catalog")]
+    GettextCatalog,
+    Glsl,
+    Go,
+    Gosu,
+    Groff,
+    Groovy,
+    [Parameter(Value = "Groovy Server Pages")]
+    GroovyServerPages,
+    Haml,
+    [Parameter(Value = "HandleBars")]
+    HandleBars,
+    Haskell,
+    Haxe,
+    Http,
+    Ini,
+    Io,
+    Ioke,
+    [Parameter(Value = "IRC log")]
+    IrcLog,
+    J,
+    Java,
+    [Parameter(Value = "Java Server Pages")]
+    JavaServerPages,
+    JavaScript,
+    Json,
+    Julia,
+    [Parameter(Value = "Jupyter Notebook")]
+    JupyterNotebook,
+    Kotlin,
+    Lasso,
+    Less,
+    Lfe,
+    [Parameter(Value = "LillyPond")]
+    LillyPond,
+    [Parameter(Value = "Literate CoffeeScript")]
+    LiterateCoffeeScript,
+    [Parameter(Value = "Literate Haskell")]
+    LiterateHaskell,
+    [Parameter(Value = "LiveScript")]
+    LiveScript,
+    Llvm,
+    Logos,
+    Logtalk,
+    Lua,
+    M,
+    Makefile,
+    Mako,
+    Markdown,
+    Matlab,
+    Max,
+    [Parameter(Value = "MiniD")]
+    MiniD,
+    Mirah,
+    Monkey,
+    Moocode,
+    Moonscript,
+    Mupad,
+    Myghty,
+    Nemerle,
+    Nginx,
+    Nimrod,
+    Nsis,
+    Nu,
+    [Parameter(Value = "NumPY")]
+    NumPY,
+    [Parameter(Value = "ObjDump")]
+    ObjDump,
+    ObjectiveC,
+    ObjectiveJ,
+    [Parameter(Value = "OCaml")]
+    OCaml,
+    Omgrofl,
+    Ooc,
+    Opa,
+    [Parameter(Value = "OpenCL")]
+    OpenCL,
+    [Parameter(Value = "OpenEdge ABL")]
+    OpenEdgeAbl,
+    Parrot,
+    [Parameter(Value = "Parrot Assembly")]
+    ParrotAssembly,
+    [Parameter(Value = "Parrot Internal Representation")]
+    ParrotInternalRepresentation,
+    Pascal,
+    Perl,
+    Php,
+    Pike,
+    [Parameter(Value = "PogoScript")]
+    PogoScript,
+    [Parameter(Value = "PowerShell")]
+    PowerShell,
+    Processing,
+    Prolog,
+    Puppet,
+    [Parameter(Value = "Pure Data")]
+    PureData,
+    Python,
+    [Parameter(Value = "Python traceback")]
+    PythonTraceback,
+    R,
+    Racket,
+    [Parameter(Value = "Ragel in Ruby Host")]
+    RagelInRubyHost,
+    [Parameter(Value = "Raw token data")]
+    RawTokenData,
+    Rebol,
+    Redcode,
+    [Parameter(Value = "reStructuredText")]
+    ReStructuredText,
+    Rhtml,
+    Rouge,
+    Ruby,
+    Rust,
+    Scala,
+    Scheme,
+    Sage,
+    Sass,
+    Scilab,
+    Scss,
+    Self,
+    Shell,
+    Slash,
+    Smalltalk,
+    Smarty,
+    Squirrel,
+    [Parameter(Value = "Standard ML")]
+    StandardML,
+    [Parameter(Value = "SuperCollider")]
+    SuperCollider,
+    [Parameter(Value = "Swift")]
+    Swift,
+    Tcl,
+    Tcsh,
+    Tea,
+    [Parameter(Value = "TeX")]
+    TeX,
+    Textile,
+    Toml,
+    Turing,
+    Twig,
+    Txl,
+    [Parameter(Value = "TypeScript")]
+    TypeScript,
+    [Parameter(Value = "Unified Parallel C")]
+    UnifiedParallelC,
+    Unknown,
+    Vala,
+    Verilog,
+    Vhdl,
+    [Parameter(Value = "VimL")]
+    VimL,
+    VisualBasic,
+    Volt,
+    Wisp,
+    Xc,
+    Xml,
+    [Parameter(Value = "XProc")]
+    XProc,
+    [Parameter(Value = "XQuery")]
+    XQuery,
+    Xs,
+    Xslt,
+    Xtend,
+    Yaml
+  }
+}
