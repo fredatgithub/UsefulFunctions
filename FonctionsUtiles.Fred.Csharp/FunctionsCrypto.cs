f44e5fa92e1ff455b75571e90bf7709f681f4b41
Fred <domaine75@gmail.com>
2023-11-26 03:01:29 +0100

    Add method to hexa

--- FonctionsUtiles.Fred.Csharp/FunctionsCrypto.cs
+++ FonctionsUtiles.Fred.Csharp/FunctionsCrypto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -562,6 +563,32 @@ namespace FonctionsUtiles.Fred.Csharp
 
         return builder.ToString();
       }
+    }
+
+    /// <summary>
+    /// Converts a string to its hexadecimal representation.
+    /// </summary>
+    /// <param name="input">The input string to convert.</param>
+    /// <returns>The hexadecimal representation of the input string.</returns>
+    public static string ToHex(this string input) => BitConverter.ToString(Encoding.UTF8.GetBytes(input)).Replace("-", "").ToLowerInvariant();
+
+    /// <summary>
+    /// Converts a hexadecimal string to its corresponding string representation.
+    /// </summary>
+    /// <param name="hex">The hexadecimal string to convert.</param>
+    /// <returns>The string representation of the hexadecimal input.</returns>
+    public static string FromHex(this string hex)
+    {
+      if (hex.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+      {
+        // (.NET8.0)
+        //hex = hex[2..];
+      }
+
+      return Encoding.UTF8.GetString(Enumerable.Range(0, hex.Length)
+          .Where(x => x % 2 == 0)
+          .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+          .ToArray());
     }
   }
 }
