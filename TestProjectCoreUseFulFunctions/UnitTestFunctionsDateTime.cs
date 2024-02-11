bfef5014bfa39401cc6fbc55a44a0ac34df0918f
Fred <domaine75@gmail.com>
2024-02-11 15:05:15 +0100

    add several unit tests

--- TestProjectCoreUseFulFunctions/UnitTestFunctionsDateTime.cs
+++ TestProjectCoreUseFulFunctions/UnitTestFunctionsDateTime.cs
@@ -7,8 +7,16 @@ namespace TestProjectCoreUseFulFunctions
   public class UnitTestFunctionsDateTime
   {
     [DataTestMethod]
-    [TestCategory("Bissextile tests")]
+    [TestCategory("Bissextile leap year tests")]
+    [DataRow(1900, false)]
+    [DataRow(2000, true)]
+    [DataRow(2020, true)]
     [DataRow(2024, true)]
+    [DataRow(2028, true)]
+    [DataRow(2100, false)]
+    [DataRow(2200, false)]
+    [DataRow(2300, false)]
+    [DataRow(2400, true)]
     public void TestMethod_Est_bissextile_2024(int year, bool expected)
     {
       bool result = FunctionsDateTime.EstBissextile(year);
