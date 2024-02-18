1cdd1365747effc535378c46c92157d2e7d9375b
Fred <domaine75@gmail.com>
2024-02-18 14:57:59 +0100

    Add crypto enumeration

---
+++ FonctionsUtiles.Fred.Csharp/Enums/AESKeySize.cs
@@ -0,0 +1,21 @@
+﻿namespace FonctionsUtiles.Fred.Csharp.Enums
+{
+  /// <summary>
+  /// AES approved cipher key sizes.
+  /// </summary>
+  public enum KeySize
+  {
+    /// <summary>
+    /// 128-bit
+    /// </summary>
+    Aes128 = 128,
+    /// <summary>
+    /// 192-bit
+    /// </summary>
+    Aes192 = 192,
+    /// <summary>
+    /// 256-bit
+    /// </summary>
+    Aes256 = 256
+  }
+}
