a1b5f205b4cdc6f66edcb8fe7bd021113ad0fcab
Fred <domaine75@gmail.com>
2024-03-20 12:27:12 +0100

    add database function class

---
+++ FonctionsUtiles.Fred.Csharp/FunctionDatabase.cs
@@ -0,0 +1,25 @@
+﻿namespace FonctionsUtiles.Fred.Csharp
+{
+  public static class FunctionDatabase
+  {
+    public static string AddTimeOutToConnectionString(string connectionString)
+    {
+      if (!connectionString.Contains("Timeout"))
+      {
+        connectionString += "Timeout=0;";
+      }
+
+      return connectionString;
+    }
+
+    public static string AddSemiColonIfNeeded(string connectionString)
+    {
+      if (!connectionString.EndsWith(";"))
+      {
+        connectionString += ";";
+      }
+
+      return connectionString;
+    }
+  }
+}
