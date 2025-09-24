224f34bb99c28b62605d1b8b2f2404912df99e51
Fred <domaine75@gmail.com>
2025-09-24 15:47:26 +0200

    app console app to test get factors

---
+++ ConsoleAppGetFactors/Program.cs
@@ -0,0 +1,25 @@
+﻿using FonctionsUtiles.Fred.Csharp;
+using System;
+
+namespace ConsoleAppGetFactors
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("Get factors of a number");
+      for (int i = 2; i < 2_000; i++)
+      {
+        var factors = FunctionsPrimes.GetFactors(i);
+        if (factors.Length != 0)
+        {
+          Display($"Les diviseurs de {i} sont : {string.Join(", ", factors)}");
+        }
+      }
+
+      Display("End of program, press a key to exit");
+      Console.ReadKey();
+    }
+  }
+}
